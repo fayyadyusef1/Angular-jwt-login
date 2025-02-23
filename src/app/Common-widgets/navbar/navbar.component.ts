aa299f55f22215dda382a5c1001fb770aac79f92
Yousef Fayyad <u8867@isoint.net>
2025-02-23 09:45:16 +0300

    Update code with new design

--- src/app/Common-widgets/navbar/navbar.component.ts
+++ src/app/Common-widgets/navbar/navbar.component.ts
@@ -1,10 +1,10 @@
 import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { SharedService } from '../../_services/shared.service';
-import { Input ,OnInit} from '@angular/core';
+import { Input, OnInit } from '@angular/core';
 import { UserDataService } from '../../_services/UserDataService.service';
 import { AuthServiceService } from '../../_services/auth-service.service';
- import { Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { User } from 'src/Models/User';
 import { TokenService } from 'src/app/_services/token.service';
 import { AuthStateService } from 'src/app/_services/auth-state.service';
@@ -20,37 +20,36 @@ import { MatDrawer } from '@angular/material/sidenav';
 export class NavbarComponent implements OnInit {
   @ViewChild('drawer') drawer: MatDrawer;
   showBox: boolean = true;
-  @Input() isSignedIn: boolean;  
-   userInfo: User;  
-   
-  
+  @Input() isSignedIn: boolean;
+  userInfo: User;
+
+
   constructor(private sharedService: SharedService,
-              public userDataService: UserDataService,
-              public authService: AuthServiceService,
-              private auth : AuthStateService,
-              private router: Router,
-              private tokenService:TokenService)
-               {
-                // console.log(this.username);
-                // console.log(username);
-                
-              }
- 
+    public userDataService: UserDataService,
+    public authService: AuthServiceService,
+    private auth: AuthStateService,
+    private router: Router,
+    private tokenService: TokenService) {
+    // console.log(this.username);
+    // console.log(username);
+
+  }
+
   private usernameSubscription: Subscription | undefined;
-  
-   user : User;
+
+  user: User;
 
   /// menu options will be active according to roleId
   /// roleId : 41 will get all menu options open , 1 admin , 3 is specialist ,4+5 are HOS and HOU with same options 
   // you can change hard coded RoleID in login.component.ts
 
-   ngOnInit(): void {
-       this.auth.userInfoState.subscribe((newUserInfo)=>{
-      this.userInfo=newUserInfo
+  ngOnInit(): void {
+    this.auth.userInfoState.subscribe((newUserInfo) => {
+      this.userInfo = newUserInfo
     });
     console.log('Initial state - isSignedIn:', this.isSignedIn, 'userRoleId:', this.userInfo.roleId);
   }
-    
+
   ngOnDestroy(): void {
     if (this.usernameSubscription) {
       this.usernameSubscription.unsubscribe();
@@ -67,33 +66,36 @@ export class NavbarComponent implements OnInit {
     this.badgevisible = true;
   }
   logout() {
-   // console.log("Logout requested");
+    // console.log("Logout requested");
     this.authService.logout();
-    localStorage.removeItem('USER_DATA');  
+    localStorage.removeItem('USER_DATA');
     this.auth.setUserInfoState(
       {
-        token:"",
-        userId:"",
-        email:"",
-        position:"",
-        upper:"",
+        token: "",
+        userId: "",
+        email: "",
+        position: "",
+        upper: "",
         status: "0",
-        aliasName:"",
+        aliasName: "",
         // username:"",
-        roleId:"",
-        roleName:"" , 
-        time:"",
+        roleId: "",
+        roleName: "",
+        time: "",
       }
-     )
-
+    )
+    // Close the side navbar after logging out
+    if (this.drawer && this.drawer.opened) {
+      this.drawer.close();
+    }
   }
   navigateToHome() {
-    this.router.navigate(['/home']);  
+    this.router.navigate(['/home']);
   }
   isAllowed(allowedRoleIds: string[]): boolean {
     // console.log('isSignedIn:', this.isSignedIn, 'userRoleId:', this.userInfo.roleId, 'allowedRoles:', allowedRoleIds);
     // Split the string of roles into an array
-  
+
     const roleArray = allowedRoleIds[0].split(',');
     return this.isSignedIn && roleArray.includes(this.userInfo.roleId.toString());
   }
@@ -101,7 +103,7 @@ export class NavbarComponent implements OnInit {
     // Check if the clicked element or its parent is disabled
     const clickedElement = event.target as HTMLElement;
     const listItem = clickedElement.closest('mat-list-item');
-    
+
     if (listItem && !listItem.hasAttribute('disabled')) {
       // This method will be called only when a non-disabled item is clicked
       // We'll use setTimeout to allow the navigation to occur before closing the drawer
@@ -112,17 +114,17 @@ export class NavbarComponent implements OnInit {
       }, 300);
     }
   }
-  
-    /// menu options will be active according to roleId
-    /// roleId : 41 will get all menu options open , 1 admin , 3 is specialist ,4+5 are HOS and HOU with same options 
-    // you can change hard coded RoleID in login.component.ts
+
+  /// menu options will be active according to roleId
+  /// roleId : 41 will get all menu options open , 1 admin , 3 is specialist ,4+5 are HOS and HOU with same options 
+  // you can change hard coded RoleID in login.component.ts
 
 
   // Roles : 
-    // 1 : Admin
-    // 2 : supplier
-    // 3 : Procurement Specialist (DEALER)
-    // 4 : HOS
-    // 5 : HOU
-    // 41 : DEVELOPER (ALL PERMISSIONS)
+  // 1 : Admin
+  // 2 : supplier
+  // 3 : Procurement Specialist (DEALER)
+  // 4 : HOS
+  // 5 : HOU
+  // 41 : DEVELOPER (ALL PERMISSIONS)
 }
